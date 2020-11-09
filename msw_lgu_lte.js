7481fa87170a1b080aea38b8927d5d64887904c2
IoTKETI <iot.keti@gmail.com>
2020-11-09 21:43:43 +0900

    - test

--- msw_lgu_lte.js
+++ msw_lgu_lte.js
@@ -231,9 +231,12 @@ function parseDataMission(topic, str_message) {
     try {
         // User define Code
         var obj_lib_data = JSON.parse(str_message);
+        console.log('origin: ', obj_lib_data);
         if(fc.hasOwnProperty('global_position_int')) {
             Object.assign(obj_lib_data, JSON.parse(JSON.stringify(fc['global_position_int'])));
         }
+        console.log('\r\n ', obj_lib_data);
+
         str_message = JSON.stringify(obj_lib_data);
 
         ///////////////////////////////////////////////////////////////////////
