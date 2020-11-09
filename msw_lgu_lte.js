953c56cf13a26652df8c43b53354b06fe4255d9b
dnjstjr93 <dnjstjr93@gmail.com>
2020-11-09 22:18:08 +0900

    - test 3

--- msw_lgu_lte.js
+++ msw_lgu_lte.js
@@ -231,11 +231,7 @@ function parseDataMission(topic, str_message) {
     try {
         // User define Code
         var obj_lib_data = JSON.parse(str_message);
-        console.log('origin: ', obj_lib_data);
         console.log('global_position_int: ', fc['global_position_int']);
-        console.log('global_position_int2: ', JSON.stringify(fc['global_position_int']));
-        console.log('global_position_int2: ', JSON.parse(JSON.stringify(fc['global_position_int'])));
-
         if(fc.hasOwnProperty('global_position_int')) {
             Object.assign(obj_lib_data, JSON.parse(JSON.stringify(fc['global_position_int'])));
         }
