fc0828c4bfbf814fc450ea93741ae70e9577317c
IoTKETI <iot.keti@gmail.com>
2021-03-26 19:53:32 +0900

    - edit file for test

--- msw_lgu_lte.js
+++ msw_lgu_lte.js
@@ -118,8 +118,8 @@ function runLib(obj_lib) {
             scripts_arr[0] = './' + config.directory_name + '/' + scripts_arr[0];
         }
 
-        var run_lib = spawn(scripts_arr[0], scripts_arr.slice(1));
-        // var run_lib = spawn('python3', [scripts_arr[0]+'.py', '/dev/ttyUSB1', '115200']);
+//         var run_lib = spawn(scripts_arr[0], scripts_arr.slice(1));
+        var run_lib = spawn('python3', [scripts_arr[0]+'.py', '/dev/ttyUSB1', '115200']);
 
         run_lib.stdout.on('data', function(data) {
             console.log('stdout: ' + data);
